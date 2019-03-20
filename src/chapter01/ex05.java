f55948a014cd3e80a525abeb71ed0bb61dc24547
eimeartwalsh <eimeartwalsh@gmail.com>
2019-03-20 17:16:31 +0000

    Added exercise 5.

---
+++ src/chapter01/ex05.java
@@ -0,0 +1,14 @@
+package chapter01;
+
+/*What happens when you cast a double to an int that is larger than the largest possible int value? Try it out.
+ */
+
+public class ex05 {
+    public static void main(String[] args){
+        System.out.println(Double.MAX_VALUE);
+        System.out.println((int)Double.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
+
+// If we try to cast a number bigger than any type int to int, program returns the largest int. 
